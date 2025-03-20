1700d1254628c1e1a1a10916a0854b8fdc627f1b
Mason Fischer <mason@kissr.co>
2025-03-20 17:39:40 -0400

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig(({ command }) => {
     outDir: "../../templates",
     rollupOptions: {
       input: {
-          magic: './src/frontend/magic.html',
+          index: './src/frontend/index.html',
       }
     }
   },
